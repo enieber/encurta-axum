f877c4c912d543d8e4c85e02f8cabc4135119a1c
Enieber Cunha <eniebercunha@gmail.com>
2025-06-20 20:49:56 -0300

    feat: create encurta url using sqlite

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+use sha256::{digest};
+use axum::{
+    http::StatusCode,
+    extract,
+    extract::{Extension, Path},
+    routing::{get,post},
+    Router,
+    Json,
+};
+use serde::{Deserialize, Serialize};
+use libsql::{params, Connection, Database};
+use std::env;
+use dotenvy::dotenv;
+use axum::response::IntoResponse;
+ 
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+            .route("/router/{hash}", get(router_to_link))
+            .route("/router", post(create_router));
+
+    let conn = db_connection().await;
+    let _ = conn
+        .execute(
+            "CREATE TABLE IF NOT EXISTS urls(link varchar non null, hash varchar non null)",
+            (),
+        )
+        .await;
+
+
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+
+#[derive(Deserialize, Serialize)]
+struct NewRouter {
+    link: String,
+}
+
+
+#[derive(Deserialize, Serialize)]
+struct GenerateRouter {
+    link: String,
+    hash: String,
+}
+
+
+fn generate_hash_link(link: String) -> String {
+    let hash = digest(link.to_string()).to_string();
+    let new_hash = &hash[0..7];
+    return new_hash.to_string()
+}
+
+
+async fn create_router(extract::Json(payload): extract::Json<NewRouter>) -> impl IntoResponse {  
+   let hash = generate_hash_link(payload.link.to_string());
+   let conn = db_connection().await;
+   let link_option = get_link_from_hash(conn.clone(), hash.clone()).await;
+   match link_option {
+       Some(link) => {
+         let router = GenerateRouter {
+           hash,
+           link: link.to_string()
+         };
+
+         return (StatusCode::OK, Json(router))
+       },
+       None => {
+         let router = GenerateRouter {
+           hash: hash.clone(),
+           link: payload.link.to_string()
+         };
+
+        let _ = conn
+        .query("INSERT into urls values (?1, ?2)", params![payload.link.clone(), hash])
+        .await;
+
+         return (StatusCode::OK, Json(router))
+     }
+   }
+}
+
+async fn get_link_from_hash(conn: Connection, hash: String) -> Option<String> {
+    let query = format!("SELECT * FROM urls WHERE hash = '{}'", hash);
+    
+    let mut results = match conn.query(query.as_str(), ()).await {
+        Ok(results) => results,
+        Err(_) => return None,
+    };
+    
+    let row = match results.next().await {
+        Ok(Some(row)) => row,
+        _ => return None,
+    };
+    
+    match row.get(0) {
+        Ok(link) => Some(link),
+        Err(_) => None,
+    }
+}
+
+async fn router_to_link(Path(hash): Path<String>) -> impl IntoResponse {
+   let conn = db_connection().await;
+   let link_option = get_link_from_hash(conn, hash.clone()).await;
+   match link_option {
+       Some(link) => {
+         let router = GenerateRouter {
+           hash,
+           link: link.to_string()
+         };
+
+         return (StatusCode::OK, Json(router))
+       },
+       None => {
+         let router = GenerateRouter {
+            hash: hash.clone(),
+            link: String::from("")
+         };
+         return (StatusCode::NOT_FOUND, Json(router))
+     }
+   }
+}
+
+async fn db_connection() -> Connection {
+    dotenv().expect(".env file not found");
+
+    let db_url = env::var("DATABASE_URL").unwrap();
+
+    let db = Database::open(db_url).unwrap();
+
+    db.connect().unwrap()
+}
